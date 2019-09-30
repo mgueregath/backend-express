a374767d03f31eed2ca017f2df656bdaa5fc8df2
Mirko Gueregat <mgueregath@gmail.com>
2019-09-30 10:52:59 -0300

    final

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-
 var express = require('express');
 var cors = require('cors');
+
 var app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cors())
 
 var PouchDB = require('pouchdb');
 var db = new PouchDB('alumnos');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.get('/alumnos', function (req, res) {
-    db.allDocs({ include_docs: true, descending: true }, function (err, docs) {
-        if (!err && docs.total_rows > 0) {
-            res.status(200).json(docs.rows.map(function (element) {
+    db.allDocs({ include_docs: true, descending: true }, function (err, doc) {
+        if (!err) {
+            res.status(200).json(doc.rows.map(function (element) {
                 return {
                     _id: element.doc._id,
                     nombre: element.doc.nombre,
@@ -28,7 +28,21 @@ app.get('/alumnos', function (req, res) {
         }
     });
 });
+app.get('/alumnos/:id', function (req, res) {
+    db.get(req.params.id, function (err, doc) {
+        if (!err) {
+            res.status(200).json({
+                _id: doc._id,
+                nombre: doc.nombre,
+                apellido: doc.apellido,
+                correo: doc.correo,
+            });
 
+        } else {
+            res.status(404).json({});
+        }
+    });
+});
 app.post('/alumnos', function (req, res) {
     var alumno = {
         _id: new Date().getTime().toString(),
@@ -36,7 +50,7 @@ app.post('/alumnos', function (req, res) {
         apellido: req.body.apellido,
         correo: req.body.correo
     };
-    db.put(alumno, function(err, result) {
+    db.put(alumno, function callback(err, result) {
         if (!err) {
             res.status(201).json(alumno);
         } else {
@@ -44,7 +58,47 @@ app.post('/alumnos', function (req, res) {
         }
     });
 });
+app.put('/alumnos/:id', function (req, res) {
+    db.get(req.params.id, function (err, doc) {
+        if (!err) {
+            doc.nombre = req.body.nombre;
+            doc.apellido = req.body.apellido;
+            doc.correo = req.body.correo;
+            db.put(doc, function callback(err, result) {
+                if (!err) {
+                    res.status(202).json({
+                        _id: doc._id,
+                        nombre: doc.nombre,
+                        apellido: doc.apellido,
+                        correo: doc.correo,
+                    });
+                } else {
+                    res.status(400).json({});
+                }
+            });
+
+
+        } else {
+            res.status(404).json({});
+        }
+    });
+});
+app.delete('/alumnos/:id', function (req, res) {
+    db.get(req.params.id, function (err, doc) {
+        if (!err) {
+            db.remove(doc, function callback(err, result) {
+                if (!err) {
+                    res.status(202).json({});
+                } else {
+                    res.status(400).json({});
+                }
+            });
+        } else {
+            res.status(404).json({});
+        }
+    });
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
